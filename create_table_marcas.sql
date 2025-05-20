3b80f9001c27f29253180ac517c1e7bdcafa24df
wolvy2004 <eduardoguerradg@gmail.com>
2025-05-20 17:47:53 -0300

    add files create_tables..

---
+++ create_table_marcas.sql
@@ -0,0 +1,7 @@
+-- pwd2025.marcas definition
+
+CREATE TABLE `marcas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) 
--- create_table_productos.sql
+++ create_table_productos.sql
@@ -5,5 +5,8 @@ CREATE TABLE `productos` (
   `descripcion` varchar(150) DEFAULT NULL,
   `precio` float(10,2) DEFAULT NULL,
   `stock` int DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `marca_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `productos_marcas_FK` (`marca_id`),
+  CONSTRAINT `productos_marcas_FK` FOREIGN KEY (`marca_id`) REFERENCES `marcas` (`id`)
 ) 
